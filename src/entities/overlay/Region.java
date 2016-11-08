90e4b3ad42c29664c562f99ec6a3f731637ac368
Nikhil Thota <nt92@ufl.edu>
2016-11-07 22:52:19 -0500

    added region stuff and cleaned repo

--- src/entities/overlay/Region.java
+++ src/entities/overlay/Region.java
@@ -4,9 +4,24 @@ import entities.board.TerrainType;
 import entities.board.TileSection;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
 
+// A region represents a specific area on the board where there is an agglomeration of a specific terrain type
+// As tiles are placed on the board, new regions are generated and tilesections are added to specific regions, while
+// the tilesections are given a region object (composition)
 public class Region {
+    private UUID tileId;
     private TerrainType terrain;
     private List<TileSection> sections;
 
+    public Region(TerrainType terrain){
+        tileId = UUID.randomUUID();
+        this.terrain = terrain;
+        sections = new ArrayList<>();
+    }
+
+    public void addSection(TileSection section){
+        sections.add(section);
+    }
 }
