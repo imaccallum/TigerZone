87972d88055ad4bbe5a404b11d61030ceea2718a
Ian MacCallum <imaccallum@ufl.edu>
2016-11-10 12:26:16 -0500

    basic factory functionality

--- src/entities/board/TileFactory.java
+++ src/entities/board/TileFactory.java
@@ -3,5 +3,41 @@ package entities.board;
 import entities.board.Tile;
 
 public class TileFactory {
-    
+
+    public Tile makeTile(char c) {
+        switch (c) {
+            case 'a': return makeTileA();
+            case 'b': break;
+            case 'c': break;
+            case 'd': break;
+            case 'e': break;
+            case 'f': break;
+            case 'g': break;
+            case 'h': break;
+            case 'i': break;
+            case 'j': break;
+            case 'k': break;
+            case 'l': break;
+            case 'm': break;
+            case 'n': break;
+            case 'o': break;
+            case 'p': break;
+            case 'q': break;
+            case 'r': break;
+            case 's': break;
+            case 't': break;
+            case 'u': break;
+            case 'v': break;
+            case 'w': break;
+            case 'x': break;
+            case 'y': break;
+            case 'z': break;
+            default: throw new RuntimeException("Invalid tile identifier");
+        }
+    }
+
+    private Tile makeTileA() {
+        Tile t = new Tile();
+        return t;
+    }
 }
