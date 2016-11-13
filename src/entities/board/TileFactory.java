e126c556a44edf91c43e1f3caad890db6683d4cd
trevorfortner <trevorfortner1@gmail.com>
2016-11-13 14:45:25 -0500

    Update TileFactory.java

--- src/entities/board/TileFactory.java
+++ src/entities/board/TileFactory.java
@@ -39,6 +39,7 @@ public class TileFactory {
         //create all the nodes for the tile, and add the nodes onto the tile in the appropriate spots in the arrays of Tile
         //make sure to account for connections (use node.setConnection(Node node)), regions (use node.setRegion(Region region); this is just creating the tiles, no need to worry about linking them yet)
         //in all of these, make sure to pass a reference to t when making the nodes for the tile
+        //we'll add a reference to Tile in node so that we can use that in Region to check if it's complete
         return t;
     }
 
