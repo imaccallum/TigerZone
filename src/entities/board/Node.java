8ad2fc150449f5e7b18cdbb5d31f29555b3da0ab
dabizaid <daar95@gmail.com>
2016-11-13 16:59:07 -0500

    Changed Folder Structure

--- src/entities/board/Node.java
+++ src/entities/board/Node.java
@@ -1,23 +1,18 @@
-package entities.board.Node;
+package entities.board;
 
-import entities.board.Tiger;
 import entities.overlay.Region;
 import entities.overlay.TileSection;
-
-import java.util.ArrayList;
 
 public class Node {
     private Node connectedNode;
     private Region region;
     private Tiger tiger;
-    private Terrain terrain;
     private TileSection tileSection;
 
     public Node(TileSection tileSection, Terrain terrain) {
         this.connectedNode = null;
         this.tiger = null;
         this.tileSection = tileSection;
-        this.terrain = terrain;
         this.region = null;
     }
     
@@ -54,13 +49,5 @@ public class Node {
 
     public void setRegion(Region region) {
         this.region = region;
-    }
-
-    public Terrain getTerrain() {
-        return terrain;
-    }
-
-    public void setTerrain(Terrain terrain) {
-        this.terrain = terrain;
     }
 }
--- src/entities/overlay/Region.java
+++ src/entities/overlay/Region.java
@@ -1,6 +1,6 @@
 package entities.overlay;
 
-import entities.board.Node.Node;
+import entities.board.Node;
 import entities.board.Tiger;
 import entities.player.Player;
 
