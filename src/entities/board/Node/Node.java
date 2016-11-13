d3fd9ca4e047a1b8d6d8617bf58b44bb519348a2
Alexander Robau <aerobau@gmail.com>
2016-11-13 15:20:28 -0500

    Adding Tile Section

--- src/entities/board/Node/Node.java
+++ src/entities/board/Node/Node.java
@@ -4,7 +4,7 @@ import entities.board.Tiger;
 import entities.overlay.Region;
 import java.util.ArrayList;
 
-public abstract class Node {
+public class Node {
     // List of all possible connections a Node might might have
     private ArrayList<Node> connectedNodes = new ArrayList<>();
 
---
+++ src/entities/board/TileSection.java
@@ -0,0 +1,5 @@
+package entities.board;
+
+public class TileSection {
+    
+}
