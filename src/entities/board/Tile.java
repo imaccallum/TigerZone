1ac6ddd9eeaa3333662b4e7318481f469a78d3f1
Ian MacCallum <imaccallum@ufl.edu>
2016-11-13 18:14:01 -0500

    this is how tiles should be

--- src/entities/board/Tile.java
+++ src/entities/board/Tile.java
@@ -15,9 +15,7 @@ public class Tile {
     private Node[] corners;
     private Node center;
     private Tile[] adjacentTiles; // Adjacent tiles
-    private int orientation; // 0 = 0, 1 = 90, 2 = 180, 3 = 270 degrees]
 
-    private boolean hasPenant;
 
     public Tile() {
         edges = new Node[COUNT];
@@ -29,94 +27,61 @@ public class Tile {
         return adjacentTiles;
     }
 
-    public Tile[] getCornerTiles() {
-        Tile[] cornerTiles = new Tile[4];
+    public Tile getTile(int index) { return adjacentTiles[index]; }
 
-        if(adjacentTiles[0] != null){
-            cornerTiles[0] = adjacentTiles[0].getAdjacentTiles()[3];
-            cornerTiles[1] = adjacentTiles[0].getAdjacentTiles()[1];
-        }
-
-        if(adjacentTiles[1] != null){
-            cornerTiles[1] = adjacentTiles[1].getAdjacentTiles()[0];
-            cornerTiles[2] = adjacentTiles[1].getAdjacentTiles()[2];
-        }
-
-        if(adjacentTiles[2] != null){
-            cornerTiles[2] = adjacentTiles[2].getAdjacentTiles()[1];
-            cornerTiles[3] = adjacentTiles[2].getAdjacentTiles()[3];
-        }
-
-        if(adjacentTiles[3] != null){
-            cornerTiles[3] = adjacentTiles[3].getAdjacentTiles()[2];
-            cornerTiles[0] = adjacentTiles[3].getAdjacentTiles()[0];
-        }
-
-        return cornerTiles;
+    public Node getCorner(int index) {
+        return corners[index];
     }
 
-    public Tile getTile(int index) { return adjacentTiles[adjustedIndex(index)]; }
-
-    public Node getCorner(CornerLocation index) {
-        return corners[adjustedIndex(index.ordinal())];
-    }
-
-    public Node getEdge(EdgeLocation index) {
-        return edges[adjustedIndex(index.ordinal())];
+    public Node getEdge(int index) {
+        return edges[index];
     }
 
     public Node getCenter(int index) {
         return center;
     }
 
-    public void setOrientation(int orientation) {
-        this.orientation = orientation % COUNT;
-    }
-
     public void rotateClockwise(int numberOfRotations) {
-        setOrientation((orientation + numberOfRotations) % COUNT);
+
+        Node[] tempEdges = new Node[COUNT];
+        Node[] tempCorners = new Node[COUNT];
+
+        for (int i = 0; i < COUNT; i++) {
+            int index = (i + numberOfRotations) % COUNT;
+            tempEdges[index] = edges[i];
+            tempCorners[index] = corners[i];
+        }
+
+        edges = tempEdges;
+        corners = tempCorners;
     }
 
-    private void setTile(Tile t, int i) throws BadPlacementException {
-        if (i < 0 || i >= COUNT) throw new BadPlacementException("Illegal index");
-        adjacentTiles[adjustedIndex(i)] = t;
-        t.getAdjacentTiles()[inverse(i)] = this;
+    private void setTile(Tile t, int index) {
+        if (index < 0 || index >= COUNT) throw new BadPlacementException("Illegal index");
+        adjacentTiles[index] = t;
     }
 
-    public void setTopTile(Tile t) throws BadPlacementException {
+    public void setTopTile(Tile t) {
         setTile(t, 0);
     }
 
-    public void setBottomTile(Tile t) throws BadPlacementException {
-        setTile(t, 2);
-    }
-
-    public void setLeftTile(Tile t) throws BadPlacementException {
-        setTile(t, 3);
-    }
-
-    public void setRightTile(Tile t) throws BadPlacementException {
+    public void setRightTile(Tile t) {
         setTile(t, 1);
     }
 
-    public void setedge(Node node, int i){
+    public void setBottomTile(Tile t) {
+        setTile(t, 2);
+    }
+
+    public void setLeftTile(Tile t) {
+        setTile(t, 3);
+    }
+
+    public void setEdge(Node node, int i){
         edges[i] = node;
     }
-    
-    public void setcorner(Node node, int i){
+
+    public void setCorner(Node node, int i){
         corners[i] = node;
-    }
-
-    // Helpers
-    private int adjustedIndex(int i) {
-        return (i + orientation) % COUNT;
-    }
-
-    private int inverse(int i) {
-        return (i + 2) % COUNT;
-    }
-
-    public boolean hasPenant() {
-        return hasPenant;
     }
 }
