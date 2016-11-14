ec4b7e666c761ca686f6e387c03a0331d6588573
Nikhil Thota <nt92@ufl.edu>
2016-11-13 19:41:14 -0500

    fixed some changes

--- src/entities/board/Tile.java
+++ src/entities/board/Tile.java
@@ -15,7 +15,7 @@ public class Tile {
     private Node[] corners;
     private Node center;
     private Tile[] adjacentTiles; // Adjacent tiles
-    private int orientation; // 0 = 0, 1 = 90, 2 = 180, 3 = 270 degrees]
+    private boolean hasPenant;
 
 
     public Tile() {
@@ -28,22 +28,40 @@ public class Tile {
         return adjacentTiles;
     }
 
-    public Tile getTile(int index) { return adjacentTiles[adjustedIndex(index)]; }
+    public Tile[] getCornerTiles() {
+        Tile[] cornerTiles = new Tile[4];
+
+        if (adjacentTiles[0] != null) {
+            cornerTiles[0] = adjacentTiles[0].getAdjacentTiles()[3];
+            cornerTiles[1] = adjacentTiles[0].getAdjacentTiles()[1];
+        }
+
+        if (adjacentTiles[1] != null) {
+            cornerTiles[1] = adjacentTiles[1].getAdjacentTiles()[0];
+            cornerTiles[2] = adjacentTiles[1].getAdjacentTiles()[2];
+        }
+
+        if (adjacentTiles[2] != null) {
+            cornerTiles[2] = adjacentTiles[2].getAdjacentTiles()[1];
+            cornerTiles[3] = adjacentTiles[2].getAdjacentTiles()[3];
+        }
+        return cornerTiles;
+    }
+
+    public Tile getTile(int index) {
+        return adjacentTiles[index];
+    }
 
     public Node getCorner(CornerLocation index) {
-        return corners[adjustedIndex(index.ordinal())];
+        return corners[index.ordinal()];
     }
 
     public Node getEdge(EdgeLocation index) {
-        return edges[adjustedIndex(index.ordinal())];
+        return edges[index.ordinal()];
     }
 
     public Node getCenter(int index) {
         return center;
-    }
-
-    public void setOrientation(int orientation) {
-        this.orientation = orientation % COUNT;
     }
 
     public void rotateClockwise(int numberOfRotations) {
@@ -62,8 +80,7 @@ public class Tile {
 
     private void setTile(Tile t, int i) throws BadPlacementException {
         if (i < 0 || i >= COUNT) throw new BadPlacementException("Illegal index");
-        adjacentTiles[adjustedIndex(i)] = t;
-        t.getAdjacentTiles()[inverse(i)] = this;
+        adjacentTiles[i] = t;
     }
 
     public void setTopTile(Tile t) throws BadPlacementException {
@@ -82,20 +99,23 @@ public class Tile {
         setTile(t, 1);
     }
 
-    public void setedge(Node node, int i){
+    public void setEdge(Node node, int i){
         edges[i] = node;
     }
     
-    public void setcorner(Node node, int i){
+    public void setCorner(Node node, int i){
         corners[i] = node;
-    }
-
-    // Helpers
-    private int adjustedIndex(int i) {
-        return (i + orientation) % COUNT;
     }
 
     private int inverse(int i) {
         return (i + 2) % COUNT;
+    }
+
+    public boolean hasPenant() {
+        return hasPenant;
+    }
+
+    public void setCenter(Node center) {
+        this.center = center;
     }
 }
--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -107,6 +107,7 @@ public class GameManager {
                 }
             }
         }
+        return currentTile;
     }
 
     private Tile iterateUp(Tile current) {
