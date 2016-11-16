f4dfbd5c2244eddeb1e6edd6778f947250595b3d
Alexander Robau <aerobau@gmail.com>
2016-11-16 14:41:51 -0500

    Revamping UML

--- src/entities/overlay/Region.java
+++ src/entities/overlay/Region.java
@@ -21,13 +21,11 @@ public class Region {
     private List<Tiger> tigers;
     private List<Region> adjacentRegions;
     private Terrain terrain;
-    private Scorer scorer;
     private int uniquePrey = 0;
     private int totalPrey = 0;
 
     public Region(Terrain terrain){
         this.terrain = terrain;
-        scorer = getScorer();
         regionId = UUID.randomUUID();
         tigers = new ArrayList<>();
         tileSections = new ArrayList<>();
