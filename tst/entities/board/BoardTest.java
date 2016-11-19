a5f09688bb91acea90ca930ba64713a7c590c538
trevorfortner <trevorfortner1@gmail.com>
2016-11-19 15:58:16 -0500

    Added comments to much of serverExample

--- tst/entities/board/BoardTest.java
+++ tst/entities/board/BoardTest.java
@@ -1,7 +1,17 @@
 package entities.board;
 
-/**
- * Created by Trevor on 11/18/2016.
- */
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BoardTest {
+    private Board b;
+
+    @Before
+    public void setup(){
+        TileFactory tileFactory = new TileFactory();
+        Tile startingTile = tileFactory.makeTile('s');
+        b = new Board(77, startingTile);
+    }
+
 }
