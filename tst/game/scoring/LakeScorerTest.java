315f8c9bb9bdf9c755179c29379249681325ef0b
trevorfortner <trevorfortner1@gmail.com>
2016-11-24 00:49:12 -0500

    Finished TrailScorerTest and added one more test to TrailScorerTest

--- tst/game/scoring/LakeScorerTest.java
+++ tst/game/scoring/LakeScorerTest.java
@@ -12,6 +12,7 @@ import java.awt.*;
 
 public class LakeScorerTest {
     private Tile rightTile;
+    private Tile bottomRightTile;
     private Tile leftTile;
     private Tile centerTile;
     private Tile bottomTile;
@@ -22,7 +23,7 @@ public class LakeScorerTest {
         factory = new TileFactory();
     }
 
-    @Test
+    @Test   //ensure closed lakes are scored as expected
     public void testClosedLakeShouldBeWorthFourPoints() throws Exception{
         setupFourPointClosedLake();
         Board board = new Board(10, leftTile);
@@ -32,7 +33,7 @@ public class LakeScorerTest {
         Assert.assertEquals(4, lakeScorer.score());
     }
 
-    @Test
+    @Test   //ensure open lakes are scored as expected
     public void testOpenLakeShouldBeWorthTwoPoints() throws Exception{
         setupTwoPointOpenLake();
         Board board = new Board(10, leftTile);
@@ -42,7 +43,7 @@ public class LakeScorerTest {
         Assert.assertEquals(2, lakeScorer.score());
     }
 
-    @Test
+    @Test   //ensure PreyAnimals affect score as necessary in open lakes
     public void testOpenLakeWithThreeUniquePreyAnimalsShouldBeWorthSixteenPoints() throws Exception{
         setupSixteenPointOpenLakeWithThreeUniquePreyAnimals();
         Board board = new Board(10,centerTile);
@@ -54,10 +55,10 @@ public class LakeScorerTest {
         Assert.assertEquals(16, lakeScorer.score());
     }
 
-    @Test
+    @Test   //ensure that LakeScorer only counts UNIQUE prey animals
     public void testOpenLakeWithOneUniquePreyAnimalsButTwoDuplicatePreyAnimalsShouldBeWorthEightPoints() throws Exception{
         // it's the same thing as testOpenLakeWithThreeUniquePreyAnimalsShouldBeWorthSixteenPoints()
-        // but it has three boars instead of one of each PreyAnimal, to check that it only counts UNIQUE prey animals
+        // but it has three boars instead of one of each PreyAnimal
         setupEightPointOpenLakeWithOneUniquePreyAnimalButTwoDuplicatePreyAnimals();
         Board board = new Board(10,centerTile);
         Point center = board.getLastPlacedTile().getLocation();
@@ -68,7 +69,7 @@ public class LakeScorerTest {
         Assert.assertEquals(8, lakeScorer.score());
     }
 
-    @Test
+    @Test   //ensure prey animals affect scoring correctly in closed lakes
     public void testClosedLakeWithTwoUniquePreyAnimalsShouldBeWorthEighteenPoints() throws Exception{
         setupEighteenPointClosedLakeWithTwoUniquePreyAnimals();
         Board board = new Board(10,centerTile);
@@ -79,11 +80,10 @@ public class LakeScorerTest {
         Assert.assertEquals(18, lakeScorer.score());
     }
 
-    @Test
+    @Test   // ensure that crocodile doesn't make the multiplier zero/negative
     public void testOpenLakeWithCrocodileButNoPreyAnimalsShouldStillBeWorthTwoPoints() throws Exception{
         // testOpenLakeShouldBeWorthTwoPoints() with rightTile having a crocodile
-        // ensure that crocodile doesn't make the multiplier zero/negative
-        setupCrocodiledOpenLakeWithNoUniquePrey();
+        setupTwoPointOpenLakeWithNoUniquePreyAndOneCrocodile();
         Board board = new Board(10, leftTile);
         Point center = board.getLastPlacedTile().getLocation();
         board.place(rightTile, new Point(center.x + 1, center.y));
@@ -91,9 +91,9 @@ public class LakeScorerTest {
         Assert.assertEquals(2, lakeScorer.score());
     }
 
-    @Test
+    @Test   //ensure one crocodile affects scoring correctly in open lakes
     public void testOpenLakeWithThreeUniquePreyAnimalsAndOneCrocodileShouldBeWorthEightPoints() throws Exception{
-        setupCrocodiledOpenLakeWithSomeUniquePrey();
+        setupEightPointOpenLakeWithTwoUniquePreyAndOneCrocodile();
         Board board = new Board(10,centerTile);
         Point center = board.getLastPlacedTile().getLocation();
         board.place(rightTile, new Point(center.x + 1, center.y));
@@ -103,9 +103,9 @@ public class LakeScorerTest {
         Assert.assertEquals(8, lakeScorer.score());
     }
 
-    @Test
+    @Test   //ensure one crocodile affects scoring correctly in closed lakes
     public void testClosedLakeWithTwoUniquePreyAnimalsAndOneCrocodileShouldBeWorthTwelvePoints() throws Exception{
-        setupCrocodiledClosedLakeWithSomeUniquePrey();
+        setupTwelvePointClosedLakeWithTwoUniquePreyAndOneCrocodile();
         Board board = new Board(10,centerTile);
         Point center = board.getLastPlacedTile().getLocation();
         board.place(rightTile, new Point(center.x + 1, center.y));
@@ -113,6 +113,20 @@ public class LakeScorerTest {
         Scorer lakeScorer = rightTile.getTileSections().get(1).getRegion().getScorer();
         Assert.assertEquals(12, lakeScorer.score());
     }
+
+    @Test   //ensure multiple crocodiles are accounted for correctly
+    public void testOpenLakeWithThreeUniquePreyAnimalsAndTwoCrocodilesShouldBeWorthTenPoints() throws Exception{
+        setupTenPointOpenLakeWithThreeUniquePreyAnimalsAndTwoCrocodiles();
+        Board board = new Board(10,centerTile);
+        Point center = board.getLastPlacedTile().getLocation();
+        board.place(rightTile, new Point(center.x + 1, center.y));
+        board.place(leftTile, new Point(center.x - 1, center.y));
+        board.place(bottomTile, new Point(center.x, center.y + 1));
+        board.place(bottomRightTile, new Point(center.x + 1, center.y + 1));
+        Scorer lakeScorer = rightTile.getTileSections().get(0).getRegion().getScorer();
+        Assert.assertEquals(10, lakeScorer.score());
+    }
+
 
     // |LJJJ---------------------LJJJ-||LJJJ---------------------LJJJ-|
     // |    X       JUNGLE      X     ||    X       JUNGLE      X     |
@@ -151,8 +165,8 @@ public class LakeScorerTest {
     //                                 |    X       JUNGLE      X     |
     //                                 |LJJJ---------------------LJJJ-|
 
+    // includes three unique prey animals and four city tiles [4*(1+3)])
     private void setupSixteenPointOpenLakeWithThreeUniquePreyAnimals(){
-        // includes three unique prey animals and four city tiles [4*(1+3)])
         centerTile = factory.makeTile("JLLL-");
         centerTile.setPreyAnimal(PreyAnimal.BOAR);
         rightTile = factory.makeTile("JLLL-");
@@ -175,8 +189,8 @@ public class LakeScorerTest {
     //                                 |    X       JUNGLE      X     |
     //                                 |LJJJ---------------------LJJJ-|
 
+    // includes three unique prey animals and four city tiles [4*(1+1)])
     private void setupEightPointOpenLakeWithOneUniquePreyAnimalButTwoDuplicatePreyAnimals(){
-        // includes three unique prey animals and four city tiles [4*(1+1)])
         setupSixteenPointOpenLakeWithThreeUniquePreyAnimals();
         rightTile.setPreyAnimal(PreyAnimal.BOAR);
         leftTile.setPreyAnimal(PreyAnimal.BOAR);
@@ -189,8 +203,8 @@ public class LakeScorerTest {
     // |    X       JUNGLE      X     ||    X       JUNGLE      X     ||    X       JUNGLE      X     |
     // |LJJJ---------------------LJJJ-||JLJL---------------------JLJL-||JLLL---------------------JLLL-|
 
+    // includes one unique prey animal and three city tiles [(3*2)*(1+1)]
     private void setupEighteenPointClosedLakeWithTwoUniquePreyAnimals() {
-        // includes one unique prey animal and three city tiles [(3*2)*(1+1)]
         centerTile = factory.makeTile("JLJL-");
         centerTile.setPreyAnimal(PreyAnimal.BOAR);
         rightTile = factory.makeTile("LJJJ-");
@@ -207,8 +221,8 @@ public class LakeScorerTest {
     // |    X        LAKE       X     ||    X       JUNGLE      X     |
     // |JLLL---------------------JLLL-||LJJJ---------------------LJJJ-|
 
-    private void setupCrocodiledOpenLakeWithNoUniquePrey() {
-        // make sure crocodile doesn't reduce multiplier to negative (two tiles, one croc ==> (2*1) = 2 Points
+    // make sure crocodile doesn't reduce multiplier to negative (two tiles, one croc ==> (2*1) = 2 Points
+    private void setupTwoPointOpenLakeWithNoUniquePreyAndOneCrocodile() {
         setupTwoPointOpenLake();
         rightTile.setHasCrocodile(true);
     }
@@ -226,8 +240,8 @@ public class LakeScorerTest {
     //                                 |LJJJ---------------------LJJJ-|
     //                                          has a Crocodile
 
-    private void setupCrocodiledOpenLakeWithSomeUniquePrey() {
-        // (four tiles, two unique prey, one croc ==> [4*(1+2-1)] = 8 Points)
+    // (four tiles, two unique prey, one croc ==> [4*(1+2-1)] = 8 Points)
+    private void setupEightPointOpenLakeWithTwoUniquePreyAndOneCrocodile() {
         setupSixteenPointOpenLakeWithThreeUniquePreyAnimals();
         centerTile.setPreyAnimal(null);
         bottomTile.setHasCrocodile(true);
@@ -240,9 +254,37 @@ public class LakeScorerTest {
     // |    X       JUNGLE      X     ||    X       JUNGLE      X     ||    X       JUNGLE      X     |
     // |LJJJ---------------------LJJJ-||JLJL---------------------JLJL-||JLLL---------------------JLLL-|
 
-    private void setupCrocodiledClosedLakeWithSomeUniquePrey(){
-        // three tiles, two unique prey, one croc ==> [(3*2)*(1+2-1)] = 12 Points)
+    // three tiles, two unique prey, one croc ==> [(3*2)*(1+2-1)] = 12 Points)
+    private void setupTwelvePointClosedLakeWithTwoUniquePreyAndOneCrocodile(){
         setupEighteenPointClosedLakeWithTwoUniquePreyAnimals();
         rightTile.setHasCrocodile(true);
+    }
+
+    //            has a Deer                      has a Boar                    has a Buffalo
+    // |LJJJ---------------------LJJJ-||JLLL---------------------JLLL-||JLLL---------------------JLLL-|
+    // |    X       JUNGLE      X     ||    X       JUNGLE      X     ||    X       JUNGLE      X     |
+    // |  JUNGLE    False      LAKE   ||   LAKE     False      LAKE   ||   LAKE     False      LAKE   |
+    // |    X       JUNGLE      X     ||    X        LAKE       X     ||    X        LAKE       X     |
+    // |LJJJ---------------------LJJJ-||JLLL---------------------JLLL-||JLLL---------------------JLLL-|
+    //                                 |LJJJ---------------------LJJJ-||LJJJ---------------------LJJJ-|
+    //                                 |    X        LAKE       X     ||    X        LAKE       X     |
+    //                                 |  JUNGLE    False     JUNGLE  ||  JUNGLE    False     JUNGLE  |
+    //                                 |    X       JUNGLE      X     ||    X       JUNGLE      X     |
+    //                                 |LJJJ---------------------LJJJ-||LJJJ---------------------LJJJ-|
+    //                                         has a crocodile                 has a crocodile
+
+    // five city tiles, three unique prey animals, two crocodiles ==> [4*(1+3-2)])
+    private void setupTenPointOpenLakeWithThreeUniquePreyAnimalsAndTwoCrocodiles(){
+        centerTile = factory.makeTile("JLLL-");
+        centerTile.setPreyAnimal(PreyAnimal.BOAR);
+        rightTile = factory.makeTile("JLLL-");
+        rightTile.setPreyAnimal(PreyAnimal.BUFFALO);
+        leftTile = factory.makeTile("LJJJ-");
+        leftTile.setPreyAnimal(PreyAnimal.DEER);
+        leftTile.rotateCounterClockwise(3);
+        bottomTile = factory.makeTile("LJJJ-");
+        bottomTile.setHasCrocodile(true);
+        bottomRightTile = factory.makeTile("LJJJ-");
+        bottomRightTile.setHasCrocodile(true);
     }
 }
